e2b7043f2a71ab54079129b5c490f9de0bf8959b
mattuu <matt@codemagix.co.uk>
2020-05-21 12:17:44 +0100

    Initial project structure

---
+++ db/schema.sql
@@ -0,0 +1,115 @@
+-- Warning: You can generate script only for one table at a time in your Free plan 
+-- 
+-- ****************** SqlDBM: Microsoft SQL Server ******************
+-- ******************************************************************
+
+CREATE TABLE [dbo].[Client]
+(
+ [ClientId] int NOT NULL ,
+ [Name]     varchar(512) NOT NULL ,
+ [TaxCode]  varchar(128) NOT NULL ,
+ [Address]  varchar(1024) NOT NULL ,
+ [Country]  varchar(256) NOT NULL ,
+
+
+ CONSTRAINT [PK_Client] PRIMARY KEY CLUSTERED ([ClientId] ASC)
+);
+GO
+
+
+CREATE TABLE [dbo].[PaymentType]
+(
+ [PaymentTypeId] int NOT NULL ,
+ [Name]          varchar(128) NOT NULL ,
+
+
+ CONSTRAINT [PK_PaymentType] PRIMARY KEY CLUSTERED ([PaymentTypeId] ASC)
+);
+GO
+
+
+CREATE TABLE [dbo].[Invoice]
+(
+ [InvoiceId]     int NOT NULL ,
+ [Year]          int NOT NULL ,
+ [Month]         int NOT NULL ,
+ [Number]        int NOT NULL ,
+ [SellDate]      datetime NOT NULL ,
+ [IssueDate]     datetime NOT NULL ,
+ [PayTime]       int NOT NULL ,
+ [IsPaid]        bit NOT NULL ,
+ [GrossAmount]   money NOT NULL ,
+ [NetAmount]     money NOT NULL ,
+ [ClientId]      int NOT NULL ,
+ [PaymentTypeId] int NOT NULL ,
+
+
+ CONSTRAINT [PK_Invoice] PRIMARY KEY CLUSTERED ([InvoiceId] ASC),
+ CONSTRAINT [FK_51] FOREIGN KEY ([PaymentTypeId])  REFERENCES [dbo].[PaymentType]([PaymentTypeId]),
+ CONSTRAINT [FK_Invoice_Client] FOREIGN KEY ([ClientId])  REFERENCES [dbo].[Client]([ClientId])
+);
+GO
+
+
+CREATE NONCLUSTERED INDEX [fkIdx_Invoice_Client] ON [dbo].[Invoice] 
+ (
+  [ClientId] ASC
+ )
+
+GO
+
+CREATE NONCLUSTERED INDEX [fkIdx_Invoice_PaymentType] ON [dbo].[Invoice] 
+ (
+  [PaymentTypeId] ASC
+ )
+
+GO
+
+
+
+-- ****************** SqlDBM: Microsoft SQL Server ******************
+-- ******************************************************************
+
+-- ************************************** [dbo].[InvoiceRow]
+
+CREATE TABLE [dbo].[InvoiceRow]
+(
+ [InvoiceRowId] int NOT NULL ,
+ [ProductName]  varchar(500) NOT NULL ,
+ [Quantity]     decimal(6,2) NOT NULL ,
+ [Unit]         varchar(50) NOT NULL ,
+ [UnitPrice]    money NOT NULL ,
+ [InvoiceId]    int NOT NULL ,
+
+
+ CONSTRAINT [PK_InvoiceRow] PRIMARY KEY CLUSTERED ([InvoiceRowId] ASC),
+ CONSTRAINT [FK_InvoiceRow_Invoice] FOREIGN KEY ([InvoiceId])  REFERENCES [dbo].[Invoice]([InvoiceId])
+);
+GO
+
+
+CREATE NONCLUSTERED INDEX [fkIdx_InvoiceRow_Invoice] ON [dbo].[InvoiceRow] 
+ (
+  [InvoiceId] ASC
+ )
+
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/SyncHR.Demo.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SyncHR.Demo.DataAccess
+{
+    public class Class1
+    {
+    }
+}
